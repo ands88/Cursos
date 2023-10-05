82320bec9eeea127983ae07fc77660d4b7f53683
ands88 <anderson.ramos13@gmail.com>
2023-10-05 08:37:32 -0300

    Update aula47_exercício.py

--- curso_python/aula47_exercício.py
+++ curso_python/aula47_exercício.py
@@ -9,30 +9,52 @@ secreta.
 Faça a contagem de tentativas do seu usuário.
 """
 
-import random 
+import random
 
-secret_words = ['dungeon', 'learning', 'programmimg', 'smart', 'cool'] # criada uma variável
-#para definir uma lista de palavras, por isso é usado os colchetes, para criar lista
-secret_word = random.choice(secret_words) # variável criada para selecionar uma palavra
-#aleatória da lista de palavras
-right_letters = [] # inicializa uma lista para armazenagem das tentativas
-times_tried = 0 # número atual de tentativas
+secret_words = [
+    "dungeon",
+    "learning",
+    "programmimg",
+    "smart",
+    "cool",
+]  # criada uma variável
+# para definir uma lista de palavras, por isso é usado os colchetes, para criar lista
+secret_word = random.choice(secret_words)  # variável criada para selecionar uma palavra
+# aleatória da lista de palavras
+right_letters = []  # inicializa uma lista para armazenagem das tentativas
+times_tried = 0  # número atual de tentativas
+tentativa = []
+i = 0
 
-print ('Quero jogar um jogo com você...')
-print ('Adivinhe a palavra secreta! Ela tem', len (secret_word), 'letras.')
+
+print("Quero jogar um jogo com você...")
+print("Adivinhe a palavra secreta! Ela tem", len(secret_word), "letras.")
+
+while i < len(secret_word):
+    str(tentativa.append("*"))
+    i += 1
+
 
 while True:
-    letter = input ('Digite uma letra: ')
-    if len (letter) != 1 or not letter.isalpha():
-        print ('Digite uma letra válida aí, parceire.')
+    print(secret_word, tentativa)
+    letter = input("Digite uma letra: ").lower()
+    if len(letter) != 1 or not letter.isalpha():
+        print("Digite uma letra válida aí, parceire.")
         continue
     if letter in right_letters:
-        print('Você já digitou essa letra, tente novamente')
+        print("Você já digitou essa letra, tente novamente")
         continue
     if letter in secret_word:
         right_letters.append(letter)
-        print('Acertou uma letra.')
+        print("Acertou uma letra.")
     else:
-        print('Letra errada, tente uma diferente')
+        print("Letra errada, tente uma diferente")
+    for i in len(secret_word):
+        if secret_word[i] == letter:
+            tentativa[i] == letter
+    if tentativa == secret_word:
+        print(
+            "Você acertou a palavra: ", secret_word, " com ", tentativa, " tentativas!"
+        )
 
     times_tried += 1
