b034a1186924cf6ebbf20e94477afc1a27966161
ands88 <anderson.ramos13@gmail.com>
2023-10-11 14:13:52 -0300

    11/10

---
+++ curso_python/aula51_desempacotamento.py
@@ -0,0 +1,8 @@
+''' 
+Introdução ao desempacotamento + tuples (tuplas)
+'''
+_, nome1, *_ = ['Maria', 'Helena', 'Anderson', 'Neuber']
+
+print (nome1, _)
+
+
---
+++ curso_python/aula52_tuplas.py
@@ -0,0 +1,9 @@
+''' 
+Tipo tupla - uma lista imutável
+'''
+
+tupla = ['Maria', 'Anderson', 'Neuber']
+tupla = tuple(tupla)
+
+print (tupla[-1])
+print (tupla)
