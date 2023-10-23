934d8fafeefecc195a10bdc68ce69000f7b5a2bb
ands88 <anderson.ramos13@gmail.com>
2023-10-23 20:57:32 -0300

    23/10

--- curso_python/aula56_split_join_strip.py
+++ curso_python/aula56_split_join_strip.py
@@ -1,22 +1,27 @@
-''' 
+""" 
 split e join com list e str
 split - divide uma string
 join - une uma string
 strip - corta espaços do começo da frase
-'''
-'''
+"""
+"""
 frase = 'Olha só que; coisa interessante'
 lista_palavras = frase.split('; ')
 
 for i, frase in enumerate(lista_palavras):
     print (lista_palavras[i].strip())
 print(lista_palavras)
-'''
+"""
 
-frase = '    Olha só que     ;     coisa interessante     '
-lista_palavras_1 = frase.split('; ')
+frase = "    Olha só que     ;     coisa interessante     "
+lista_palavras_1 = frase.split("; ")
 lista_palavras = []
 
-for i, frase in enumerate(lista_palavras):
-   lista_palavras [i] = lista_palavras[i].strip()
-print(lista_palavras)
+for i, frase in enumerate(lista_palavras_1):
+    lista_palavras.append(lista_palavras_1[i].strip())
+
+# print(lista_palavras_1)
+# print(lista_palavras)
+
+frase_unida = " ".join(lista_palavras)
+print(frase_unida)
