6278bf1d9df072a2fb89e28d68671bcfc488550d
ands88 <anderson.ramos13@gmail.com>
2023-10-23 21:18:01 -0300

    23/10

---
+++ curso_python/aula57_iteravel_dentro_iteravel.py
@@ -0,0 +1,12 @@
+""" Lista de listas e seus índices
+"""
+salas = [
+    ["Maria", "Marcos", "Solange"],
+    ["Anderson", (0, 10, 20, 30, 40)],
+    ["Neuber", "Aderivaldo", "Luiz"],
+]
+
+print(salas[1][0])
+print(salas[0][1])
+print(salas[2][2])
+print(salas[1][1][2])
