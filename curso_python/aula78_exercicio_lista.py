9f943a42ede1ce98c665e9026769dd3b4a7f299c
ands88 <anderson.ramos13@gmail.com>
2023-11-13 13:24:54 -0300

    13/11

--- curso_python/aula78_exercicio_lista.py
+++ curso_python/aula78_exercicio_lista.py
@@ -1,32 +1,53 @@
 # Exercício - sistema de perguntas e respostas
 
-questions = [
+exam = [
     {
         "Pergunta": "Quanto é 2 * 2",
-        "Opções": ["6", "8", "2", "4"],
-        "Resposta": "4",
+        "Opções": ["a) 6", "b) 8", "c) 2", "d) 4"],
+        "Resposta": "d",
     },
     {
         "Pergunta": "Qual o nome do satélite natural da Terra?",
-        "Opções": ["Europa", "Galaxia", "Lua", "Sol"],
-        "Resposta": "Lua",
+        "Opções": ["a) Europa", "b) Galaxia", "c) Lua", "d) Sol"],
+        "Resposta": "c",
     },
     {
         "Pergunta": "Quanto é 1 + 6?",
-        "Opções": ['7', '5', '6', '1'],
-        "Resposta": "7",
+        "Opções": ['a) 7', 'b) 5', 'c) 6', 'd) 1'],
+        "Resposta": "a",
     },
     {
         "Pergunta": "Quanto é 2 * 55?",
-        "Opções": ["100", "150", "110", "125"],
-        "Resposta": "110",
+        "Opções": ["a) 100", "b) 150", "c) 110", "d) 125"],
+        "Resposta": "c",
     },
 ]
 
 
+counter_right = 0
 
-print(f'Responda:', questions[1]['Pergunta'])
 
+print(f'Questão:', exam[0]['Pergunta'])
+
+for alternative in exam[0]['Opções']:
+    print (alternative)
+
+answer_check = input ('Digite a alternativa: ')
+#transformar a alternativa em letra minúscula para checagem
+if answer_check.isupper():
+    answer_check = answer_check.lower()
+
+#checar resposta da questão 1 (tester o while, enquanto o counter_right for menor do que o len (exame))
+if exam[0]['Resposta'] == answer_check and answer_check.isalpha():
+    print('Correto!')
+    counter_right += 1
+elif exam[0]['Resposta'] != answer_check:
+    print ('Errou!')
+else:
+    print('Digite uma alternativa válida')
+
+
+    
 # função para fazer pergunta
 def asking():
     ...
