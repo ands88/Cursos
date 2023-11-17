a31ab65ad17da9d147e47437e5cf651aed6bcd87
ands88 <anderson.ramos13@gmail.com>
2023-11-16 21:57:02 -0300

    16/11

--- curso_python/tempCodeRunnerFile.py
+++ curso_python/tempCodeRunnerFile.py
@@ -1,2 +1,18 @@
-    else:
-        print("Digite uma alternativa válida")
+def find_number(lista):
+    ocorrencias = set()
+
+    for numero in lista:
+        if numero in ocorrencias:
+            return numero
+        ocorrencias.add(numero)
+
+    return -1
+
+
+resultados = []
+
+for lista in lista_de_listas_de_inteiros:
+    resultado = find_number(lista)
+    resultados.append(resultado)
+
+print(resultados)
