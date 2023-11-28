d0a173692f05a31b9f83a0604c2eb8c857cc693e
ands88 <anderson.ramos13@gmail.com>
2023-11-28 13:36:13 -0300

    28/11

--- curso_python/aula101_exercício_yield.py
+++ curso_python/aula101_exercício_yield.py
@@ -1,23 +1,22 @@
 # Exercício - Adiando execução de funções
 def soma(x, y):
-    yield x + y
+    return x + y
 
 
 def multiplica(x, y):
-    yield x * y
+    return x * y
 
 
-def criar_funcao(funcao, *args):
-    yield funcao(*args)
+def criar_funcao(funcao, x):
+    def interna (y):
+        return funcao(x,y)
+    return interna
 
 
 soma_com_cinco = criar_funcao(soma, 5)
 multiplica_por_dez = criar_funcao(multiplica, 10)
 
-resultado_soma = soma_com_cinco(10)
-resultado_multiplica = multiplica_por_dez(4)
-
-print(resultado_soma)
+print('Soma:', soma_com_cinco(7))
 print()
-print(resultado_multiplica)
+print('Multiplicação:', multiplica_por_dez(23))
 
