127a11bc3c322c55c049c0c6f882575eda2bf13d
ands88 <98363269+ands88@users.noreply.github.com>
2023-12-08 06:16:15 -0300

    08/12

--- curso_python/aula109_combination_permutation_product.py
+++ curso_python/aula109_combination_permutation_product.py
@@ -2,8 +2,9 @@
 # Combinação - Ordem não importa - iterável + tamanho do grupo
 # Permutação - Ordem importa
 # Produto - Ordem importa e repete valores únicos
+from itertools import combinations
 
 pessoas = ['Anderson', 'Neuber', 'João', 'Carlos']
 camisetas = ['preta', 'branca']
 
-
+print(*list(combinations(pessoas,2)), sep='\n')
