108e533ef6e845f7d66460a46d188279fe28dafd
ands88 <anderson.ramos13@gmail.com>
2023-12-21 13:40:01 -0300

    20/12

--- curso_python/aula116_context_manager.py
+++ curso_python/aula116_context_manager.py
@@ -1,3 +1,4 @@
+import os
 # Criando arquivos com Python + Context Manager with
 # Usamos a função open para abrir
 # um arquivo em Python (ele pode ou não existir)
@@ -26,6 +27,39 @@ file_path += 'arquivo116.txt'
 
 #arquivo.close()
 
-with open(file_path, 'w') as arquivo:
-    print("Olá mundo!")
-    print("Esse arquivo ser[a fechado")
+# with open(file_path, 'w+') as arquivo:
+#     arquivo.write('Linha 1\n')
+#     arquivo.write('Linha 2\n')
+#     arquivo.writelines(
+#         ('Linha 3\n', 'Linha 4\n')
+#     )
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('Lendo....')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='') #utilizado para remover a quebra de linha no final 
+#     print(arquivo.readline().strip()) #utilizado para remover a quebra de linha no final 
+#     print(arquivo.readline().strip())
+#     print("READLINES")
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+# print('#' * 20)
+
+# with open(file_path, 'r') as arquivo:
+#     print(arquivo.read())
+
+with open(file_path, 'w') as arquivo: #usando o "w", quando é executado, o arquivo é totalmente apagado e escrito novamente
+    arquivo.write('Atenção\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(
+        ('Linha 3\n', 'Linha 4\n')
+    )
+
+#no windows, o encoder tem que ser "chamado" no código para não dar erro.
+# with open(file_path, 'w', encoder='utf-8') as arquivo:
+    
+# os.unlink(file_path) #Apaga o arquivo 
+# os.rename(file_path, 'aula116-2.txt') #renomeia o arquivo
---
+++ curso_python/aula117_json.py
@@ -0,0 +1,18 @@
+person = {
+    "nome": "Anderson",
+    "sobrenome": "Martins",
+    "altura": 1.82,
+    "numeros_preferidos": (3, 8, 9),
+    "dev": True,
+    "nada": None,            
+    "endereços": [
+        {
+            "rua": "22", 
+            "numero": 53
+        }, 
+        {
+            "rua": "J-1", 
+            "numero": 90
+        }
+    ]
+}
